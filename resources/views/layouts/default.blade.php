06506fa6155650b4f09f392165b15655c56d7b20
omar96developer <omakino@hotmail.it>
2021-12-22 17:25:58 +0100

    0.1

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <header>
+        @include('partials.the_header')
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+        @include('partials.the_footer')
+    </footer>  
+</body>
+</html>
